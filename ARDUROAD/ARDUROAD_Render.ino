f55fe4c1d6539ce398ee63275676a4fdb3b46357
Filmote <simon@bloggingadeadhorse.com>
2018-06-21 22:20:50 +1000

    WIP5

--- ARDUROAD/ARDUROAD_Render.ino
+++ ARDUROAD/ARDUROAD_Render.ino
@@ -386,10 +386,8 @@ void RenderScreen(uint8_t gear) {
     uint8_t day = level.getDay();
 
     Sprites::drawPlusMask(16, 15, StartOfDay, 0);
-    // Sprites::drawOverwrite(69, 25, font_images, 52 + (day / 10));
-    // Sprites::drawOverwrite(74, 25, font_images, 52 + (day % 10));
-    Sprites::drawOverwrite(69, 26, font_images, 52 + (day / 10));
-    Sprites::drawOverwrite(74, 27, font_images, 52 + (day % 10));
+    Sprites::drawOverwrite(69, 25, font_images, 52 + (day / 10));
+    Sprites::drawOverwrite(74, 25, font_images, 52 + (day % 10));
 
   }
 
